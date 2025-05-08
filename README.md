d9190a6036bd08f5f464c056964518d6887264b7
Kiril Arsov <kirilarsov@gmail.com>
2025-05-08 09:21:02 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # docker-container-availability-exporter
-Pre processor for the docker-events in the context of creating availability report in Kibana
+
+Service for listening on given docker events and pushing them directly to the ELK stack
+
+# Usage
+
+This service is designed to be run as a docker container.
+
+# Design
+
+This is a simple service that functions as a pre-processor for the docker events in order to specifically create
+availability report even for the same containers that can be started and stopped multiple times.
+
+It uses a simple cache mechanism to store the events and when the container is stopped, it will push the event to the
+ELK stack.
+
+The service is using the Python `elasticsearch` library to send events to the ELK stack. The service is designed to be
+run as a docker container. The service is using the `docker` library to listen on docker events. The service is using
+the
+`docker` library to listen on docker events. More regarding the library
+here https://elasticsearch-py.readthedocs.io/en/v8.18.1/
+
+
---
+++ app.py
@@ -0,0 +1,46 @@
+import docker
+from elasticsearch import Elasticsearch
+from collections import defaultdict, deque
+from datetime import datetime
+
+es = Elasticsearch(hosts=['http://elk:9200'])
+client = docker.from_env()
+
+# Cache to track active start events: {(container_id, image_name): deque(start_times)}
+start_cache = defaultdict(deque)
+
+def sanitize_keys(obj):
+    if isinstance(obj, dict):
+        return {k.replace('.', '_'): sanitize_keys(v) for k, v in obj.items()}
+    return obj
+
+def send_combined_event(container_id, image_name, start_time, end_time):
+    doc = {
+        "containerId": container_id,
+        "imageName": image_name,
+        "availableStartDate": datetime.fromtimestamp(start_time).strftime('%b %d, %Y @ %H:%M:%S.%f')[:-3],
+        "availableEndDate": datetime.fromtimestamp(end_time).strftime('%b %d, %Y @ %H:%M:%S.%f')[:-3] if end_time else "-"
+    }
+    try:
+        es.index(index='container-availability', document=sanitize_keys(doc))
+    except Exception as e:
+        print(f"Indexing error: {str(e)}")
+
+for event in client.events(decode=True):
+    if event['Type'] == 'container' and event['status'] in ('start', 'die'):
+        actor = event['Actor']
+        container_id = actor['ID']
+        image_name = actor['Attributes'].get('image', 'unknown')
+        timestamp = event['time']
+
+        if event['status'] == 'start':
+            start_cache[(container_id, image_name)].append(timestamp)
+
+        elif event['status'] == 'die':
+            cache_key = (container_id, image_name)
+            if start_cache[cache_key]:
+                start_time = start_cache[cache_key].popleft()
+                send_combined_event(container_id, image_name, start_time, timestamp)
+            else:
+                # can handle orphaned die events
+                pass
